94a16bb2a980aeba93ec9258e24560b52e28351d
Marc Laughton <marclaughton@Marc-Laughton-MBP-15.local>
2018-02-05 14:58:04 -0500

    Use buildpack to create aws config dirs

--- bin/install_awscli.sh
+++ bin/install_awscli.sh
@@ -10,18 +10,6 @@ arrow() {
   sed -u 's/^/-----> /'
 }
 
-APP_DIR="/app"
-INSTALL_DIR="$APP_DIR/vendor/awscli"
-BUNDLE_DIR="$APP_DIR/vendor/awscli-bundle"
-
-echo "Installing awscli..." | arrow
-chmod +x "$BUNDLE_DIR/install"
-"$BUNDLE_DIR/install" --install-dir "$INSTALL_DIR" --bin-location "$APP_DIR/bin/aws" | indent
-chmod u+x "$INSTALL_DIR/bin/aws"
-
-echo "Updating PATH"
-export PATH=~/vendor/awscli/bin:$PATH
-
 echo "Configuring awscli in ~/.aws" | arrow
 mkdir ~/.aws
 
@@ -35,3 +23,7 @@ cat >> ~/.aws/config << EOF
 [default]
 region = $AWS_REGION
 EOF
+
+echo "It will be automatically run on dyno launch." | indent
+echo "Make sure to set AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY and AWS_REGION variables in ENV." | indent
+echo
